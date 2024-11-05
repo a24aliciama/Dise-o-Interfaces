7d6998ec84a0bb6e51ea58a85f03a27a1bf5c3d5
aliciaMA <a24aliciaMA@iessanclemente.net>
2024-11-05 08:55:22 +0100

    dd

--- Pruebas/Prueba1_RestauranteBelga/WebBarPostresBelga/script/Index.js
+++
@@ -1,8 +0,0 @@
-
-// JavaScript para alternar el estado de expansión del menú
-const botonColapsador = document.getElementById("botonColapsador");
-const menuPadre = document.getElementById("menuPadre");
-
-botonColapsador.addEventListener("click", () => {
-    menuPadre.classList.toggle("expandido");
-});
